2b2cb54126c2954ec3d7667da29d70357da5640c
Anh <anhoev@yahoo.de>
2019-01-05 17:28:48 +0700

    fix delete

--- index.js
+++ index.js
@@ -137,6 +137,9 @@ function walk (root, cb, immutable) {
                 if (stopHere) keepGoing = false;
             },
             'delete' : function (stopHere) {
+                this.parent.after(function() {
+                    this.update(this.node.filter(e => e));
+                });
                 delete state.parent.node[state.key];
                 if (stopHere) keepGoing = false;
             },
